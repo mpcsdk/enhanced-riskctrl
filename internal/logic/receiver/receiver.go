38f65eac327f15b51020a1d19a5cbaa380bde5cf
frank <no>
2024-06-17 17:16:50 +0800

    latest transfer

--- internal/logic/receiver/receiver.go
+++ internal/logic/receiver/receiver.go
@@ -6,9 +6,11 @@ import (
 	"enhanced_riskctrl/internal/conf"
 	"enhanced_riskctrl/internal/service"
 
+	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcmd"
 	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/lib/pq"
 	"github.com/mpcsdk/mpcCommon/mpcdao"
 	"github.com/mpcsdk/mpcCommon/mpcdao/model/entity"
 	"github.com/mpcsdk/mpcCommon/mq"
@@ -22,6 +24,16 @@ type sReceiver struct {
 	///
 	enhanced_riskctrl *mpcdao.EnhancedRiskCtrl
 	mpc               *mpcdao.MpcContext
+}
+
+func isDuplicateKeyErr(err error) bool {
+	gerr := err.(*gerror.Error)
+	if cerr, ok := gerr.Cause().(*pq.Error); ok {
+		if cerr.Code == "23505" {
+			return true
+		}
+	}
+	return false
 }
 
 func new() *sReceiver {
@@ -42,7 +54,7 @@ func new() *sReceiver {
 	nats := mq.New(conf.Config.Nrpc.NatsUrl)
 	jet := nats.JetStream()
 
-	cons, err := nats.GetConsumer("riksctrl", mq.JetStream_SyncChain, mq.JetSub_SyncChainTransfer)
+	cons, err := nats.GetConsumer("riksctrl", mq.JetStream_SyncChain, mq.JetSub_SyncChainTransfer_Latest)
 	if err != nil {
 		panic(err)
 	}
@@ -90,15 +102,19 @@ func new() *sReceiver {
 		///
 		err = s.enhanced_riskctrl.InsertTx(ctx, tx)
 		if err != nil {
-			g.Log().Error(ctx, "insertdb :", tx, ", err:", err)
+			if isDuplicateKeyErr(err) {
+				g.Log().Warning(ctx, "insertdb isDuplicateKeyErr:", tx, ", err:", err)
+			} else {
+				g.Log().Fatal(ctx, "insertdb:", tx, ", err:", err)
+			}
+		} else {
+			///aggval
+			err = s.enhanced_riskctrl.AggTx(ctx, tx)
+			if err != nil {
+				g.Log().Fatal(ctx, "agg tx:", tx, ", err:", err)
+			}
 		}
-		g.Log().Debug(ctx, "insertdb :", tx)
 
-		///aggval
-		err = s.enhanced_riskctrl.AggTx(ctx, tx)
-		if err != nil {
-			g.Log().Fatal(ctx, "agg tx:", tx, ", err:", err)
-		}
 		g.Log().Info(ctx, "check mpcaddr record:", tx.From, tx.ChainId, tx.Height)
 		msg.Ack()
 		//
